be5cf6cc0ee2a00aa2a695bc544b5d13455906df
OmarMWarraich <earthling986@gmail.com>
2022-09-26 04:07:01 +0500

    feat: Add context api and initial state

---
+++ src/context/AppContext.js
@@ -0,0 +1,10 @@
+const initialState = {
+    budget: 10000,
+    expenses: [
+        {id: 1, name: 'Shopping', cost: 40},
+        {id: 2, name: 'Holiday', cost: 400},
+        {id: 3, name: 'Transportation', cost: 60},
+        {id: 4, name: 'Fuel', cost: 70},
+        {id: 5, name: 'Food', cost: 120},
+    ],
+};
